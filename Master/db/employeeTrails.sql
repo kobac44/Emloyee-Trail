4ca05093765b10805e0583ff6b417ff6c1d78693
Unko <kobie.watkins@gmail.com>
2020-08-10 01:46:14 -0400

    need to finish

--- Master/db/employeeTrails.sql
+++ Master/db/employeeTrails.sql
@@ -1,35 +1,89 @@
--- DROP DATABASE IF EXISTS employeeTrails_db;
--- CREATE DATABASE employeeTrails__db;
+DROP DATABASE IF EXISTS employeeTrails_db;
+CREATE DATABASE employeeTrails__db;
 
--- USE employeeTrails__db;
+USE employeeTrails__db;
 
--- CREATE TABLE department
--- (
---       department_id INT NOT NULL
---       AUTO_INCREMENT PRIMARY KEY,
---   department_name VARCHAR
---       (30) NOT NULL
---   );
+CREATE TABLE department
+(
+    department_id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR
+    (30) NOT NULL
+  );
 
---       CREATE TABLE role
---       (
---             role_id INT NOT NULL
---             AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR
---             (30),
---   salary DECIMAL
---             (10,2),
---   department_id INT NOT NULL
---   );
+    CREATE TABLE role
+    (
+        role_id INT NOT NULL
+        AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR
+        (30),
+  salary DECIMAL
+        (10,2),
+  department_id INT NOT NULL,
+  foreign key
+        (department_id) references department
+        (id) ON
+        DELETE CASCADE
+)ENGINE =INNODB;
 
---             CREATE TABLE employee
---             (
---                   emplyee_id INT NOT NULL
---                   AUTO_INCREMENT PRIMARY KEY,
--- first_name VARCHAR
---                   (30) NOT NULL,
--- last_name VARCHAR
---                   (30) NOT NULL,
--- role_id INT NOT NULL,
--- manager INT NOT NULL
--- );
+        CREATE TABLE employee
+        (
+            emplyee_id INT NOT NULL
+            AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR
+            (30) NOT NULL,
+last_name VARCHAR
+            (30) NOT NULL,
+role_id INT NOT NULL,
+foreign key
+            (role_id) references role
+            (id) ON
+            DELETE CASCADE,
+manager_id integer(30),
+foreign key
+            (manager_id) references employee
+            (id)ON
+            DELETE CASCADE
+)ENGINE =INNODB;
+
+
+
+            select *
+            from role;
+
+            select *
+            from employee;
+
+            select *
+            from department;
+
+            INSERT into DEPARTMENT
+                (name)
+            values
+                ("Engineering"),
+                ("Sales"),
+                ("Finance"),
+                ("Legal")
+            ;
+
+            INSERT into ROLE
+                (title, salary, department_id)
+            values("Sales Lead" , 15, 1),
+                ("Salesperson", 9, 2),
+                ("Lead Engineer", 150, 3),
+                ("Software Engineer", 16, 4),
+                ("Accountant", 7, 1),
+                ("Legal", 250000, 2),
+                ("Lawyer", 190000, 3);
+
+            INSERT into EMPLOYEE
+                (first_name, last_name, role_id, manager_id)
+            values
+                ('John', 'Doe', 1, NULL),
+                ('Mike', 'Chan', 2, 1),
+                ('Ashley', 'Rodriguez', 3, NULL),
+                ('Kevin', 'Tupik', 4, 3),
+                ('Kunal', 'Singh', 5, NULL),
+                ('Malia', 'Brown', 6, 5),
+                ('Sarah', 'Lourd', 7, NULL),
+                ('Tom', 'Allen', 8, 7);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -10,14 +11,16 @@ const connection = mysql.createConnection({
   database: 'employeeTrails'
 });
 
+function runChoices() {
 
-function start() {
+  console.log("Connected to the database!");
   inquirer.prompt([
     {
       name: 'action',
       type: 'list',
       message: 'What would you like to do?',
-      choices: ['Add Employee',
+      choices: [
+        'Add Employee',
         'Remove Employee',
         'View All Employees',
         'Add Employee Role',
@@ -26,10 +29,9 @@ function start() {
         'Add Employee By Department',
         'View Employee By Department',
         'Delete Employee by Department',
-        'Update Employee Roles',
+        'Update Employee Roles'
       ]
     }
-
   ]).then(function (answer) {
     switch (answer.action) {
       case 'Add Employee':
@@ -40,13 +42,320 @@ function start() {
         removeEmployee();
         break;
 
+      case 'View All Employees':
+        viewAllEmployees();
+        break;
 
+      case 'Add Employee Role':
+        addRole();
+        break;
 
-    }
-  }
-  )
+      case 'View Employee Role':
+        viewRole();
+        break;
+
+      case 'Add Employee By Department':
+        addDepartment();
+        break;
+
+      case 'View Employee by Department':
+        viewDepartment();
+        break;
+
+      case 'Update Employee Role':
+        updateRoll();
+        break;
+
+      case 'Delete Employee Role':
+        deleteRole();
+        break;
+
+      case 'Delete Employee by Department':
+        deleteDepartment();
+        break;
+
+    };
+  });
 };
-start();
+
+function addEmployee() {
+  let listNames = ["None"];
+  connection.query('select first_name from employee', function (err, data) {
+    if (err);
+    for (let i = 0; i < data; i++) {
+      listNames.push(data[i].first_name);
+    }
+
+  })
+  inquirer.prompt([
+    {
+      name: 'first',
+      type: 'input',
+      message: 'Enter the first name of the employee',
+    },
+    {
+      name: 'last',
+      type: 'input',
+      message: 'Enter the last name',
+
+    },
+    {
+      name: 'erole',
+      type: 'list',
+      message: 'What is employee role?',
+      choices: [
+        'Software Engineer',
+        'Software Tester',
+        'Lead Engineer',
+        'Sales Lead',
+        'Sales Person',
+        'Lawyer'
+      ]
+    },
+    {
+      name: 'managerName',
+      type: 'list',
+      message: 'Who is the employee manager?',
+      choices: listNames
+    }
+
+  ]).then(function (response) {
+    connection.query(`insert into employee (first_name,last_name,role_id,manager_id) values (?,?,(select id from role where title=?),(select id from employee e where e.first_name=?))`, [response.first, response.last, response.erole, response.managerName],
+      function (err, result) {
+        console.log(result);
+      })
+  })
+}
+
+function addRole() {
+  let roles = [];
+  connection.query('select title from role', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      roles.push(data[i].title);
+    }
+
+    inquirer.prompt([
+      {
+        name: 'title',
+        type: 'list',
+        message: 'What is employee role?',
+        choices: roles
+      },
+
+      {
+        name: 'salary',
+        type: 'list',
+        message: 'Enter the salary'
+
+      },
+
+      {
+        name: 'deptName',
+        type: 'input',
+        message: 'Enter the department name'
+      }
+
+    ]).then(function (response) {
+      connection.query(`insert into role (title,salary,department_id) values(?,?,(select id from department where name =?))`, [response.title, response.salary, response.deptName],
+        function (err, result) {
+          if (err) throw err;
+          console.log(result);
+        })
+    })
+  })
+}
+
+function addDepartment() {
+  let deptNames = [];
+
+  connection.query('select name from department', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      deptNames.push(data[i].name);
+    }
+
+    inquirer.prompt([
+      {
+        name: 'deptName',
+        type: 'list',
+        message: 'What department does the employee belong to?',
+        choices: deptNames
+      }
+    ]).then(function (response) {
+      connection.query('insert into department set ?',
+        {
+          name: response.deptName
+          //role:response.erole
+        }
+        , function (err, result) {
+          if (err) throw err;
+          console.log(result);
+        })
+    })
+  })
+
+}
+
+function viewDepartment() {
+  connection.query('select * from department', function (err, result) {
+    if (err) throw err;
+    console.table(result);
+  })
+
+}
+
+function viewRole() {
+  connection.query('select * from role', function (err, result) {
+    if (err) throw err;
+    console.table(result);
+  })
+
+
+}
+
+function viewAllEmployees() {
+  connection.query('select e.id,e.first_name,e.last_name,r.title,r.salary,d.name from employee e join role r on e.role_id=r.id join department d on r.department_id=d.id', function (err, result) {
+    if (err) throw err;
+    console.table(result);
+  })
+
+}
+
+function updateRoles() {
+  let updateNames = [];
+  connection.query('select first_name from employee', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      updateNames.push(data[i].first_name);
+    }
+
+
+    let roles = [];
+    connection.query('select title from role', function (err, data) {
+      if (err) throw err;
+      for (let i = 0; i < data.length; i++) {
+        roles.push(data[i].title);
+      }
+
+      inquirer.prompt([
+        {
+          name: 'empUpdate',
+          type: 'list',
+          message: 'Which employee do you want to update?',
+          choices: updateNames
+        },
+
+        {
+          name: 'title',
+          type: 'list',
+          message: 'What is the role to update with?',
+          choices: roles
+        }
+
+
+      ]).then(function (response) {
+        connection.query(`update role set title=? where id= (select role_id from employee where first_name=?)`, [response.title, response.empUpdate],
+          function (err, result) {
+            if (err) throw err;
+            console.log(result);
+          })
+      })
+    })
+
+  })
+
+}
+
+function removeEmployee() {
+
+  let deleteList = [];
+  connection.query('select first_name from employee', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      deleteList.push(data[i].first_name);
+    }
+
+
+    inquirer.prompt([
+      {
+        name: 'empDelete',
+        type: 'list',
+        message: 'Which employee do you want to remove?',
+        choices: deleteList
+      }
+
+    ]).then(function (response) {
+      connection.query(`delete from employee where first_name=?`, [response.empDelete],
+        function (err, result) {
+          if (err) throw err;
+          console.table(result);
+        })
+    })
+  })
+
+
+}
+
+
+function deleteRole() {
+  let deleteRo = [];
+  connection.query('select title from role', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      deleteRo.push(data[i].title);
+    }
+
+    inquirer.prompt([
+      {
+        name: 'delRole',
+        type: 'list',
+        message: 'Which employee role do you want to remove?',
+        choices: deleteRo
+      }
+
+    ]).then(function (response) {
+      connection.query(`delete from role where title=?`, [response.delRole],
+        function (err, result) {
+          if (err) throw err;
+          console.table(result);
+        })
+    })
+  })
+
+
+}
+
+function deleteDepartment() {
+  let deleteDept = [];
+  connection.query('select name from department', function (err, data) {
+    if (err) throw err;
+    for (let i = 0; i < data.length; i++) {
+      deleteDept.push(data[i].name);
+    }
+
+    inquirer.prompt([
+      {
+        name: 'dep',
+        type: 'list',
+        message: 'Which department do you want to remove?',
+        choices: deleteDept
+      }
+
+    ]).then(function (response) {
+      connection.query(`delete from department where name=?`, [response.delRole],
+        function (err, result) {
+          if (err) throw err;
+          console.table(result);
+        })
+    })
+  })
 
 
 
+}
+
+//connection.end();
+
+
+runChoices();
