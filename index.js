cd798b832a1507bf10652ed01a815ea6b226e88e
Unko <kobie.watkins@gmail.com>
2020-08-14 23:42:33 -0400

    something is wrong with my machine cutting off, ending tonights work 11:42pm

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function runChoices() {
 function viewAllEmployees() {
   connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;",
     function (err, res) {
-      if (err) throw err;
+      if (err);
       for (var i = 0; i < res.length; i++) {
         console.log("Id: " + res[i].id + " Name: " + res[i].first_name + " " + res[i].last_name + " || Title: " + res[i].title + " || Salary: " + res[i].salary + " || Dept: " + res[i].name);
       };
